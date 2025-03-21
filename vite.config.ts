38aa10146a10b79b56b6f978bcc6fd74bd2f9d89
thoma axha <thomaaxha@hotmail.com>
2025-03-21 01:22:47 +0100

    Set base path for deployment in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/gertmetani-profil",
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ['lucide-react']    
   },
 });
